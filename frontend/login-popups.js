4b2c8be399363eca012a5d95df6b2552e7e13163
AJDColvin <ajdc21@bath.ac.uk>
2024-12-20 18:19:40 +0000

    Login/Register forms write to database

---
+++ frontend/login-popups.js
@@ -0,0 +1,38 @@
+async function login(){
+    const username = document.getElementById('login-username').value;
+    const password = document.getElementById('login-password').value;
+
+    const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+        localStorage.setItem('token', data.token); // Save the JWT token
+        alert(data.message);
+    } else {
+        alert(data.message);
+    }
+
+}
+
+async function register(){
+    // might not be able to get like this. Look into forms in HTML.
+    const username = document.getElementById('register-username').value;
+    const password = document.getElementById('register-password').value;
+    console.log("TEST")
+
+    const response = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    }); 
+
+    const data = await response.json();
+    alert(data.message);
+    // alert('TESTING');
+    
+}
+
