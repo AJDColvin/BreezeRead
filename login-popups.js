2ead2ae4a92c47a2ee45ab26a23063bea32f81f2
AJDColvin <ajdc21@bath.ac.uk>
2024-12-20 23:46:15 +0000

    http -> https for fetch url

--- login-popups.js
+++ login-popups.js
@@ -2,7 +2,7 @@ async function login(){
     const username = document.getElementById('login-username').value;
     const password = document.getElementById('login-password').value;
 
-    const response = await fetch('http://breezereadbe-production.up.railway.app/login', {
+    const response = await fetch('https://breezereadbe-production.up.railway.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
@@ -24,7 +24,7 @@ async function register(){
     const password = document.getElementById('register-password').value;
     console.log("TEST")
 
-    const response = await fetch('http://breezereadbe-production.up.railway.app/register', {
+    const response = await fetch('https://breezereadbe-production.up.railway.app/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
