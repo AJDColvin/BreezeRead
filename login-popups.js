07ad9979668a0296e32ccc3501c4d0719b3e9a88
AJDColvin <ajdc21@bath.ac.uk>
2024-12-21 22:46:44 +0000

    Logged in functionality

--- login-popups.js
+++ login-popups.js
@@ -1,9 +1,30 @@
+let loggedIn = false;
+
 const apiBaseUrl =
     window.location.hostname === '127.0.0.1'
         ? 'http://localhost:3000' // Local backend
         : 'https://breezereadbe-production.up.railway.app'; // Railway backend
 
+function checkLoggedIn() {
+    if (loggedIn) {
+        loggedInPopup();
+    }
+    else {
+        openPopup('popup-login');
+    }
+}
+
+function loggedInPopup(){
+    openPopup('popup-logged-in');
+    const username = document.getElementById('login-username').value;
+    console.log(username);
+    document.getElementById("username_placeholder").innerHTML = `${username}`;
+    console.log(`${username}`);
+
+}
+
 async function login(){
+    //username not defined globally?
     const username = document.getElementById('login-username').value;
     const password = document.getElementById('login-password').value;
 
@@ -16,7 +37,10 @@ async function login(){
     const data = await response.json();
     if (response.ok) {
         localStorage.setItem('token', data.token); // Save the JWT token
+        loggedIn = true;
         alert(data.message);
+        closePopup('popup-login');
+        loggedInPopup();
     } else {
         alert(data.message);
     }
@@ -36,7 +60,14 @@ async function register(){
 
     const data = await response.json();
     alert(data.message);
-
     
 }
+
+function logout(){
+
+    loggedIn = false;
+    closePopup('popup-logged-in');
+
+}
+
 
